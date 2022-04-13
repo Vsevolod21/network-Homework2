920bb5d7ef70dd94ec3d67296b6fb8729a4fd639
Vsevolod21 <kononenko.vsevolod@gmail.com>
2022-04-13 14:44:29 +0300

    Version homework2

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,11 +4,13 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class Client {
     public static void main(String[] args) {
-        String host = "localhost";
+        String host = "netology.homework";
+
         int port = 8075;
 
         try (Socket clientSocket = new Socket(host, port);
@@ -16,9 +18,21 @@ public class Client {
                      PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new
                      InputStreamReader(clientSocket.getInputStream()))) {
-            out.println("Чувак!");
-            String resp = in.readLine();
-            System.out.println(resp);
+
+            String serverMessage1 = in.readLine();
+            System.out.println(serverMessage1);
+
+            out.println("Чувак");
+
+            String serverMessage2 = in.readLine();
+            System.out.println(serverMessage2);
+
+            out.println("Yes");
+
+            String serverMessage3 = in.readLine();
+            System.out.println(serverMessage3);
+
+            out.println("Thank you ever so much!");
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,11 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         System.out.println(("Сервер стартовал"));
         int port = 8075;
 
@@ -18,14 +20,26 @@ public class Main {
                     (clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader
                     (new InputStreamReader(clientSocket.getInputStream()));
-
             System.out.printf("Новое соединение принято. Порт: %d%n",
                     clientSocket.getPort());
 
-            final String name = in.readLine();
+            out.println("Write your name");
+
+            String username = in.readLine();
+            System.out.println(username);
+
+            out.println("Are you child? (yes/no)");
+
+            String clientMessage2 = in.readLine();
+            System.out.println(clientMessage2);
+
             out.println(String.format
-                    ("Привет, %s, твой порт %d", name, clientSocket.getPort()));
+                    ("Welcome to the kids area, %s! Let's play!", username));
+
+            String clientMessage3 = in.readLine();
+            System.out.println(clientMessage3);
             serverSocket.close();
+            break;
         }
     }
 }
